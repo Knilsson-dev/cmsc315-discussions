1f848556cf7201f57d816ff8527ea370a27f9980
kaleb nilsson <KalebnilssoN@yahoo.com>
2026-09-20 19:22:55 -0700

    Discussion 6 push

--- unit6_hash_tables/unit6_discussion.py
+++ unit6_hash_tables/unit6_discussion.py
@@ -34,6 +34,17 @@ def main():
 
     print("\n=== INSERT OPERATIONS ===")
     print("TODO: Create a dictionary and add multiple key-value pairs.")
+    dictionary = {}
+    dictionary["Jenna"] = 90
+    dictionary["Nick"] = 65
+    dictionary["Dominique"] = 34
+    dictionary["Chelsea"] = 76
+    dictionary["Drew"] = 13
+    #Dictionary behaves like a hash table because its pythons build in implementation of a hash table. Every time i add a key
+    #dictionary["Nick"]=x python runs that key through a hash function which then converts it into a number which is then used to calculate the index to the array
+
+    print(f'Dictionary: {dictionary}')
+
 
     # ===============================
     # TODO (Student): LOOKUP OPERATIONS
@@ -46,6 +57,9 @@ def main():
 
     print("\n=== LOOKUP OPERATIONS ===")
     print("TODO: Demonstrate successful key lookups.")
+    print(f"Lookup: {dictionary['Jenna']}")
+    print(f"Lookup: {dictionary['Nick']}")
+    #pythons build in dict function hashes the key which then goes to that index and then checks to verify that the key actually matches and then returns the value if key match = true
 
     # ===============================
     # TODO (Student): UPDATE OPERATIONS
@@ -59,6 +73,14 @@ def main():
 
     print("\n=== UPDATE OPERATIONS ===")
     print("TODO: Demonstrate updating an existing key.")
+    print(f"Dictionary before update: {dictionary}")
+    dictionary["Dominique"] = 11
+    print(f"Updated dictionary: {dictionary}")
+    print(f"Dictionary before update: {dictionary}")
+    dictionary["Chelsea"] = 67
+    print(f"Updated dictionary: {dictionary}")
+    #Since the key already exists python hashes is and then lands on the same exact index used the first time the key was added.
+    #INstead of creating a new entry it just overwrites the value that was being stored. This allows the key to stay in place and only changes the value
 
     # ===============================
     # TODO (Student): DELETE OPERATIONS
@@ -72,6 +94,13 @@ def main():
     print("\n=== DELETE OPERATIONS ===")
     print("TODO: Demonstrate deleting a key-value pair.")
 
+    print(f"Dictionary before delete: {dictionary}")
+    del dictionary["Jenna"]
+    print(f"Deleted dictionary: {dictionary}")
+    print(f"Dictionary before delete: {dictionary}")
+    del dictionary["Drew"]
+    print(f"Deleted dictionary: {dictionary}")
+    #Similar to the  above instead of searching the whole list the key is hashed by python leading to the exact index in which the value and the key are then removed.
     # ===============================
     # TODO (Student): EDGE CASES
     # ===============================
@@ -88,8 +117,17 @@ def main():
 
     print("\n=== EDGE CASES ===")
     print("TODO: Demonstrate and explain edge cases.")
+    #Edge case 1 lookup a missing key
+    try:
+        print(f"Edge Case: {dictionary['Kaleb']}")
+    except KeyError as e:
+        print(f"Edge Case: KeyError: {e} does not exist in dictionary")
+    #In this edge case the key is missing so up reaching this line of code a KeyError: 'Kaleb' Is thrown
 
-
+    #Edge case 2 update  missing key
+    dictionary["Kaleb"] = 10
+    print(f"Edge Case: {dictionary['Kaleb']}")
+    #in this case with updating a key that is missing python just adds it to the hashtable
 
 if __name__ == "__main__":
     main()
